09ba1727e411816551a50f065f6b0f5f3bb0b7ac
EduardoVega04 <eduarvega04@hotmail.com>
2020-04-29 04:50:42 +0000

    Convert lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    var = ord('a') - ord('A')
+    for i in str:
+        print("{}".format(chr(ord(c) - toUpper) if islower(c) else c), end='')
+    print()

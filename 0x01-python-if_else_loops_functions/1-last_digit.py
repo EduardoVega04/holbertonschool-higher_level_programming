7e3b7af967887db86069c534b68500aa23fe8b85
EduardoVega04 <eduarvega04@hotmail.com>
2020-04-29 01:09:43 +0000

    Get the abs and last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-abs_value = abs(number)
-last = abs_value % 10
-mensaje = "The last digit of {:d} ".format(number)
-mensaje2 = "is {:d} and is ".format(last)
+last = abs(number) % 10
 
+if number < 0:
+    last = last * -1
+
+mensaje1 = "The last digit of"
 if last > 5:
-    print(mensaje + mensaje2 + "greater than 5")
+    mensaje = "and is greather than 5"
 elif last == 0:
-    print(mensaje + mensaje2 + "zero")
+    mensaje = "and is zero"
 else:
-    print(mensaje + mensaje2 + "less than 6 and not 0")
+    mensaje = "and is less than 6 and not 0"
+
+print("{:s} {:d} is {:d} {:s}".format(mensaje1, number, last, mensaje))

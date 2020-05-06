bf7944998da63f91ae8ee8a713301fbc4cce39e7
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-06 02:04:29 +0000

    Print a 2D matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(' '.join(map(str, row)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print("{:d}".format(matrix[i][j]), end='')
+            if j < (len(matrix[i]) - 1):
+                print(' ', end='')
+        print()

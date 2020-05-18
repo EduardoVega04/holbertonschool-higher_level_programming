6b3444ceccef20abed14f8b419bc8e8497ed9bda
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-18 23:30:26 +0000

    IndexError exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+        try:
+            for i in range(x):
+                print(a[i], end='')
+        except IndexError:
+            print("\nError, out of index")
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+<strong>Exception handling in Python<strong>

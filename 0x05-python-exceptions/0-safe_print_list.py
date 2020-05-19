dda1dbf0e14d93e15820bb7e1e6000ba53f816ce
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-19 00:08:58 +0000

    IndexError handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     cont = 0
     for i in range(x):
         try:
-            print(my_list[i], end='')
+            print(my_list[i], end="")
             cont += 1
         except IndexError:
             pass
-        finally:
-            print("")
-            return (cont)
+    print("")
+    return (cont)

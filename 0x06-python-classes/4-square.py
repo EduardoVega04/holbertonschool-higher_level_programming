50997df1f5570b10a25d5e6107f64b409507f233
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-20 04:11:23 +0000

    Create a class with setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Create a class Square"""
+
+
+class Square:
+    """Creates our beautiful square with some attributes"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Function to retrieve size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function for size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = value

de7d9bb0a73b9dc2a7d6aa0f8c4f67ae50d12cdc
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-20 02:38:55 +0000

    Create a class with private attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,6 @@
 
 class Square:
     """Our class Square, with the following attributes:"""
-        def __init__(self, size=0):
-            """Now we initialize the size attribute as private"""
-            self.__size = size
+    def __init__(self, size=0):
+        """Now we initialize the size attribute as private"""
+        self.__size = size

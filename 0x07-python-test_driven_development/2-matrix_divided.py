a50673136e196130cfc5a60d87ac1215283042e3
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-23 07:55:42 +0000

    Problem 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,23 +14,4 @@ def matrix_divided(matrix, div):
 
     """
 
-    if (not isinstance(matrix, list) or
-            not all(isinstance(lists, list) for lists in matrix)):
-        raise (TypeError
-               ('matrix must be a matrix (list of lists) of integers/floats'))
-
-    if not all(all(isinstance
-                   (i, (float, int)) for i in row) for row in matrix):
-        raise (TypeError
-               ("matrix must be a matrix (list of lists) of integers/floats"))
-
-    if not all(len(x) == len(matrix[0]) for x in matrix):
-        raise TypeError('Each row of the matrix must have the same size')
-
-    if not isinstance(div, (int, float)):
-        raise TypeError('div must be a number')
-
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
-
-    return ([list(map(lambda n: round(n/div, 2), i)) for i in matrix])
+    
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This function will say the input name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function to print: My name is <first name> <last name>
+
+    Args:
+        first_name (str): The first name
+        last_name (str): The last name
+
+    Returns:
+        Nothing. It prints: My name is <first name> <last name>
+
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("first_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))

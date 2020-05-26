f724e9f98755912fab78492679090538a17ab193
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-26 03:59:01 +0000

    Create a class Rectangle with getter, setter and other methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for the width of the rectangle"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter for the height of the rectangle"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -42,11 +42,11 @@ class Rectangle:
 
     def area(self):
         """Returns the area of the rectangle"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return (2 * (self__width + self.__height))
+            return (2 * (self.__width + self.__height))

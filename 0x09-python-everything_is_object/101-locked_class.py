bb9a18c88fd97d7a0160f1fab0afafadf37dff6b
EduardoVega04 <eduarvega04@hotmail.com>
2020-05-27 04:00:05 +0000

    Answers problems 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class LockedClass . Low memory cost"""
+
+class LockedClass:
+    """Only allows the user to create an attribute called first_name"""
+
+    __slots__ = ["first_name"]

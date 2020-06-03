442f6f68c5303930b2aae04e2036613ad61fd3f5
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 04:44:22 +0000

    Create a class with a function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Initialize"""
+
+
+class BaseGeometry:
+    """Our beautiful class"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")

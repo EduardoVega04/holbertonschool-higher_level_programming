798e52a1e5ac38eb029562900b01d78673b92d28
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 04:10:14 +0000

    Use dir() to get attribute list of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Returns a list of attributes of an object"""
+
+
+def lookup(obj):
+    """Our function. Returns a list of object attributes"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+<strong>Inheritance in Python<strong>

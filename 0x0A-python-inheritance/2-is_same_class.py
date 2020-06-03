46dbf5cd366bf10b8aae6677e98f9c39056bba23
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 04:29:43 +0000

    The isinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Initialize"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if the object is exactly an instance of the class"""
+    return (isinstance(obj) is a_class)

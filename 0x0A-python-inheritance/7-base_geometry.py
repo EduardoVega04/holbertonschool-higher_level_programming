5d0107a9a54f00fe371fa2b219c8459343d23de7
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 04:52:40 +0000

    A class with methods and exceptions handling

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Initialize"""
+"""initialize module"""
 
 
-class BaseGeomtry:
-    """Our beautiful class"""
+class BaseGeometry:
+    """create class"""
+
     def area(self):
-        """Raises an exception"""
+        """create instance public"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the value"""
+        """validator"""
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(name + " must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater that 0".format(value))
+            raise ValueError(name + " must be greater than 0")

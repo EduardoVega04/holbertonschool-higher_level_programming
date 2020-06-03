6e43ce8c149f042ee5770ab71d32d791304cea30
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 04:19:12 +0000

    A class that inherits list data type attributes

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class that inherits from list data type"""
+
+
+class MyList(list):
+    """The main class"""
+    def print_sorted(self):
+        """Inherits everything from class list (datatype)"""
+        return (sorted(self))

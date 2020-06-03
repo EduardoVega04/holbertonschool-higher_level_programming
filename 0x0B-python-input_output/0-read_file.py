7a770c7b7b1d0ec81c6082604394fdec18de841d
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-03 23:25:38 +0000

    Read an entire file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,8 @@
 
 import os
 
+
 def read_file(filename=""):
     """Reads an entire file at once (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as myFile:
         print(myFile.read())
-

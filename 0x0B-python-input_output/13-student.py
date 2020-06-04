97e07bce52e19899fee311c5fdbf21db909c9b6f
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-04 04:44:00 +0000

    Creates a student class, retrieves its  attributes in a dictionay representation  and also replaces them

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create class Student, retrieve a dictionary representation of an instance"""
+
+
+class Student:
+    """Our class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of an instance"""
+        dic = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if hasattr(self, i):
+                dic[i] = getattr(self, i)
+        return dic
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            if hasattr(self, k):
+                setattr(self, k, v)

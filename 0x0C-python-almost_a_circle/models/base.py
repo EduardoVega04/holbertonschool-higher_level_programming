9f6c0c76e24d7c5a95201b8b135ce04eb0c3d5f3
EduardoVega04 <eduarvega04@hotmail.com>
2020-06-10 04:45:56 +0000

    Load from file and return a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,54 +8,51 @@ import json
 
 
 class Base:
-    """A base class"""
+    """Our base class for this project"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize the base class"""
+        """Constructor for this class"""
         if id is None:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
         else:
             self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of a list of dictionaries"""
-        if list_dictionaries is None:
-            list_dictionaries = []
-        return json.dumps(list_dictionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return []
-        return json.loads(json_string)
+        return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
+        """Writes the JSON string representation of list_objs to a file"""
+        objs = []
         filename = cls.__name__ + ".json"
-        lo = []
-        if list_objs is not None:
+
+        if not (list_objs is None):
             for i in list_objs:
-                lo.append(cls.to_dictionary(i))
-        with open(filename, 'w') as f:
-            f.write(cls.to_json_string(lo))
+                objs.append(cls.to_dictionary(i))
+
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(objs))
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
-        if cls.__name__ is "Rectangle":
+        """Returns an instance with all the attributes already set"""
+        if cls.__name__ == 'Rectangle':
             dummy = cls(1, 1)
-        elif cls.__name__ is "Square":
+        elif cls.__name__ == 'Square':
             dummy = cls(1)
+
         dummy.update(**dictionary)
-        return dummy
+        return (dummy)
 
     @classmethod
     def load_from_file(cls):
+        """Returns a list of instances of type caller class from a file"""
         filename = cls.__name__ + ".json"
         l = []
         try:

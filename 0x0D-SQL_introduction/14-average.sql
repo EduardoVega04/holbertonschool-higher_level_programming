45fa331d1322d6a12c93ef43adbf5880426cc86e
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-15 02:04:40 +0000

    Compute the average of scores in a new column

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
 -- Script that computes the score average of all records in the table second_table
-ALTER TABLE second_table ADD average FLOAT;
-num = SELECT AVG(score) FROM second_table;
-INSERT INTO second_table (average) VALUES (num);
+SELECT AVG(score) FROM second_table

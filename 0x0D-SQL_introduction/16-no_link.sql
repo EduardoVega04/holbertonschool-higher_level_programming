3cf35a250bf479e6621b851c8d2792988daa36f3
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-15 02:37:29 +0000

    List all records of the table that have a name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name FROM second_table
-WHERE name IS NOT NULL
-ORDER BY DESC;
+SELECT score, name FROM second_table WHERE name != NULL ORDER BY score DESC;

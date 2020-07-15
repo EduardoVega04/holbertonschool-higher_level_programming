2afd012ff452408618857eeb97f9cd4b60797015
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-15 03:41:36 +0000

    Grant all privileges to two users in localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @ 'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2' @ 'localhost';
+SHOW GRANTS user_0d_1;
+SHOW GRANTS user_0d_2

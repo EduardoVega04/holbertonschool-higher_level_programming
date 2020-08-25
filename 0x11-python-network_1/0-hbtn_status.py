1a00cc4a174c09f9044c7cf1ceb29bfc4bc55cf6
EduardoVega04 <60398965+EduardoVega04@users.noreply.github.com>
2020-08-24 23:42:27 -0500

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""initializate"""
-from urllib.request import urlopen
+""" Default doctstring. """
+import urllib.request
 
-
-def hbtn_status_0():
-    """function show my status"""
-    with urlopen('https://intranet.hbtn.io/status') as response:
+if __name__ == "__main__":
+    my_url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(my_url) as response:
         html = response.read()
-        utf8 = html.decode('utf-8')
-        print("Body response:\n\t- type: {}".format(type(html)))
-        print("\t- content: {}\n\t- utf8 content: {}".
-              format(html, utf8, end=""))
 
-
-if __name__ == '__main__':
-    hbtn_status_0()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))

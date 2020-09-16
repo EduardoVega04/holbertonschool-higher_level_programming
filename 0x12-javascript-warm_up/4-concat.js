15d770de3c27aeee6898f81c4cda85774cbfbede
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-15 22:50:34 -0500

    Print argvs

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 /* Prints two arguments passed to it, in the following format: “ is ” */
 
-let myArg1 = 'undefined';
-let myArg2 = 'undefined';
+let myArg1 = process.argv[2];
+let myArg2 = process.argv[3];
 
-if (process.argv[2]) {
-  myArg1 = process.argv[2];
-}
-if (process.argv[3]) {
-  myArg2 = process.argv[3];
-}
 console.log(myArg1, 'is', myArg2);

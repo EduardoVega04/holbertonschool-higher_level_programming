3057ed86f9b85338a46746fb4e37a8bbf2567e57
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-15 23:59:54 -0500

    task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,7 +2,7 @@
 // JS Script
 
 exports.callMeMoby = function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-	theFunction();
-    }
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };

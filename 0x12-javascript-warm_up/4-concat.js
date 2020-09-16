f1a1fd7b30bc073de4bf6c5efb29f3bc2997869a
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-15 22:51:23 -0500

    Concatenate argvs

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Prints two arguments passed to it, in the following format: “ is ” */
 
-let myArg1 = process.argv[2];
-let myArg2 = process.argv[3];
+const myArg1 = process.argv[2];
+const myArg2 = process.argv[3];
 
 console.log(myArg1, 'is', myArg2);

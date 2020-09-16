a813239c9cab99b8f3fd11d5d1e272516fa798fd
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-15 20:19:43 -0500

    Handle argv in javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 /* Handling argv, argc */
-const args = process.argv;
 
-if (args[2]) {
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }

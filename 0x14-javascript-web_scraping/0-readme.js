cd9caebda6db7290921bd8438d4e240e1542123c
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-22 21:31:50 -0500

    Read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 /* Script that reads and prints the content of a file */
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {

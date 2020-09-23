5e101aaaa542bb30ff57de8997c072300322ae58
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-23 01:34:36 -0500

    Request from an API

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const filmIndex in films) {
-      const filmChars = films[filmIndex].characters;
-      for (const charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
-        }
+const req = require('request');
+let counter = 0;
+let body = '';
+
+req(process.argv[2], function (error, stat, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    body = JSON.parse(data);
+    const results = body.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].match('/18/')) { counter += 1; }
       }
     }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log(counter);
   }
 });

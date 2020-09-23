94f8bbe91275c00986265a673b645381ef4487ce
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-22 22:01:31 -0500

    Perform a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 /* Display status code of a GET request */
 const https = require('https');
-https.get(process.argv[2], (res) => {
-  console.log('code:', res.statusCode);
+https.get(process.argv[2], (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', res.statusCode);
+  }
 });

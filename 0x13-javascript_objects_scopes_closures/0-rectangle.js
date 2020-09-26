c37e1c0e645db68767a1e847d3de2a6c70810344
EduardoVega04 <eduarvega04@hotmail.com>
2020-09-25 22:25:26 -0500

    Create an empty class

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-/* Empty class that defines a rectangle */
-class Rectangle {
-    constructor(){        
-    }
-}
+/* Empty class that defines a rectangle and can be imported elsewhere */
+module.export = class Rectangle {}
